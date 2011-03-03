3faa5ccddea0acd86e993ad8d626645403c5b18b
Samantha John <samanthajohn@Samantha-Johns-MacBook-Pro.local>
2011-03-02 23:46:04 -0500

    domain name generator script

---
+++ domains.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+root_word = ARGV[0] || ''
+word_size = ARGV[1].to_i || 3
+domain_names = []
+dictionary = File.read('/usr/share/dict/words').split("\n").select{|d| d.size == word_size}
+dictionary.each do |word|
+	domain_names.push("#{word}#{root_word}.com")
+end
+system("touch #{root_word}.txt"}
+names = File.open("#{root_word}.txt", 'w')
+domain_names.each do |name|
+	names.write("#{name}\n")
+end
+names.close
